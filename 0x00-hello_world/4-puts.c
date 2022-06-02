746c6a2e5d276880f08941a9219d97381b6010a8
Andile <maendykhuzwayo@gmail.com>
2022-06-02 12:48:47 -0700

    P

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
  *    *  **/
 int main (void)
 {
-	lputs("\" Programming is like building a multilingual puzzle");
+	puts("\" Programming is like building a multilingual puzzle");
 
 	Return (0);
 }

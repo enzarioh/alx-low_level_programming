4d2688520cb85317c9ea66aee77fd487b1518062
Andile <maendykhuzwayo@gmail.com>
2022-06-02 13:16:44 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,46 @@
+#Include <stdion.h>
+/**
+ *
+ *  *  * Main - Begining 
+ *
+ *   *    *
+ *
+ *    *     *Return Always 0 (Success)
+ *
+ *     **/
+
+int main(void)
+
+{
+
+
+
+	int a;
+
+	long in b;
+
+	long long int c;
+
+	char d;
+
+	float f;
+
+
+
+	printf(Size of a char : %lu byte9s0\n", (unassigned long)sizeoff(d));
+
+	printf(Size of a int : %lu byte9s0\n", (unassigned long)sizeoff(a));
+
+	printf(Size of a long int : %lu byte9s0\n", (unassigned long)sizeoff(b));
+
+	printf(Size of a long long  : %lu byte9s0\n", (unassigned long)sizeoff(c));
+
+	printf(Size of a float : %lu byte9s0\n", (unassigned long)sizeoff(f));
+
+
+
+	return (0);
+
+
+
+}
